79992fbf270c947699d4588d12a24425e1e0c015
parimala.killada <parimala.killada@rockets.utoledo.edu>
2017-06-28 19:38:23 +0000

    flask folder

---
+++ flask/app/views.py
@@ -0,0 +1,58 @@
+
+
+from flask import render_template, request
+from app import app
+
+from forms import AddressForm
+import yaml
+import psycopg2
+from pandas import read_sql
+from getpass import getpass
+
+
+#pwd = getpass('password')
+
+with open('./app/config.yml','r') as fin:
+  config = yaml.load(fin)
+
+
+
+def create_conn(config):
+    try:
+      con=psycopg2.connect(dbname=config['dbname'], host=config['host'],
+                              port=config['port'], user=config['user'],
+                             password =config['password'])
+   
+      return con
+    except Exception as err:
+        print(err)
+
+
+@app.route('/')
+def main():
+    return render_template("index.html",city = '',procedure ='')
+
+@app.route('/', methods =["POST","GET"])
+def index():
+    con = create_conn(config)
+    cur = con.cursor()
+    city = request.form['city']
+    
+    procedure = request.form['procedure']
+    query = "select * from hospprocoder where city =\'{}\' and  proceduredescription = \'{}\' order by year;".format(city,procedure)
+    print(query)
+    
+    cur.execute(query)
+    results = cur.fetchall()
+    return render_template("index.html",city = city,procedure = procedure,results = results)
+
+#@app.route('/submit', methods=('GET', 'POST'))
+#def submit():
+    
+ #   form = AddressForm()
+  #  if form.validate_on_submit():
+   #     return redirect('index.html',form =form)
+   # return render_template('index.html', form=form)
+
+
+
